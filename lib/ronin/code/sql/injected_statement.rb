78ce44583226bccb696519d70fe178d219610c46
postmodern <postmodern.mod3@gmail.com>
2008-10-22 16:22:17 -0700

    Added the no_rows method.

--- lib/ronin/code/sql/injected_statement.rb
+++ lib/ronin/code/sql/injected_statement.rb
@@ -52,6 +52,10 @@ module Ronin
           inject_and(BinaryExpr.new('=',value,value))
         end
 
+        def no_rows
+          inject_and(BinaryExpr.new('=',1,0))
+        end
+
         def has_field?(name)
           inject_or(field(name).is_not?(null))
         end
