d935082b60f3b5af7006657087eee2b6ed52efe8
postmodern <postmodern.mod3@gmail.com>
2008-10-29 15:24:24 -0700

    Added some more specs.

--- spec/code/sql/create_examples.rb
+++ spec/code/sql/create_examples.rb
@@ -13,4 +13,14 @@ shared_examples_for "Create" do
       :users
     ]
   end
+
+  it "should have a temp option" do
+    @sql.temp
+    @sql.instance_variable_get('@temp').should == true
+  end
+
+  it "should have a if_not_exists option" do
+    @sql.if_not_exists
+    @sql.instance_variable_get('@if_not_exists').should == true
+  end
 end
