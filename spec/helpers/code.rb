0f6d8ea17f3755419eddad9b5fdbd68477ac1ad4
postmodern <postmodern.mod3@gmail.com>
2008-10-29 16:33:58 -0700

    Added the should_have_clause helper.

--- spec/helpers/code.rb
+++ spec/helpers/code.rb
@@ -7,3 +7,8 @@ include Code::SQL
 def common_dialect
   Dialect.get(:common).new
 end
+
+def should_have_clause(sql,name,&block)
+  sql.has_clause?(name).should == true
+  block.call(sql.get_clause(name)) if block
+end
