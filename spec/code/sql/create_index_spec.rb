33fa24f1728fa6f16482230b46c6b30c63dfd713
postmodern <postmodern.mod3@gmail.com>
2008-10-29 15:18:30 -0700

    Added a CreateIndex spec.

---
+++ spec/code/sql/create_index_spec.rb
@@ -0,0 +1,22 @@
+require 'ronin/code/sql/create_table'
+require 'ronin/code/sql/program'
+
+require 'code/sql/create_examples'
+
+describe CreateIndex do
+  before(:each) do
+    @sql = CreateIndex.new(common_dialect)
+  end
+
+  it_should_behave_like "Create"
+
+  it "should have an on clause" do
+    @sql.on :users, [:name]
+
+    @sql.has_clause?(:on).should == true
+
+    on = @sql.get_clause(:on)
+    on.table.should == :users
+    on.fields.should == [:name]
+  end
+end
