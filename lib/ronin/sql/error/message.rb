6118c2643f1925be3e2a50a403244e6306488d40
postmodern <postmodern.mod3@gmail.com>
2008-11-04 13:29:44 -0800

    Moved error.rb and error_pattern.rb into the error/ directory.

--- lib/ronin/sql/error/message.rb
+++ lib/ronin/sql/error/message.rb
@@ -23,40 +23,42 @@
 
 module Ronin
   module SQL
-    class Error
+    module Error
+      class Message
 
-      # The URL which is vulnerable
-      attr_reader :url
+        # The URL which is vulnerable
+        attr_reader :url
 
-      # The vulnerable query param
-      attr_accessor :param
+        # The vulnerable query param
+        attr_accessor :param
 
-      # SQL error type
-      attr_reader :type
+        # SQL error type
+        attr_reader :type
 
-      # SQL Dialect
-      attr_reader :dialect
+        # SQL Dialect
+        attr_reader :dialect
 
-      # SQL error message
-      attr_reader :message
+        # SQL error message
+        attr_reader :message
 
-      #
-      # Creates a new SQL Error object with the specified _type_, 
-      # _dialect_ and _message_.
-      #
-      def initialize(type,dialect,message)
-        @type = type
-        @dialect = dialect
-        @message = message
+        #
+        # Creates a new SQL Error object with the specified _type_, 
+        # _dialect_ and _message_.
+        #
+        def initialize(type,dialect,message)
+          @type = type
+          @dialect = dialect
+          @message = message
+        end
+
+        #
+        # Returns the message in String form.
+        #
+        def to_s
+          @message.to_s
+        end
+
       end
-
-      #
-      # Returns the message in String form.
-      #
-      def to_s
-        @message.to_s
-      end
-
     end
   end
 end
---
+++ lib/ronin/sql/error/pattern.rb
@@ -0,0 +1,103 @@
+require 'ronin/sql/error/message'
+
+module Ronin
+  module SQL
+    module Error
+      class Pattern
+
+        # Name of the pattern
+        attr_reader :name
+
+        # Name of the SQL dialect
+        attr_accessor :dialect
+
+        # Patterns to use for matching SQL errors
+        attr_reader :patterns
+
+        #
+        # Creates a new ErrorPattern object with the specified _name_. If a
+        # _block_ is given, it will be passed the newly created ErrorPattern
+        # object.
+        #
+        def initialize(name,&block)
+          @dialect = :common
+          @name = name.to_sym
+          @patterns = []
+
+          block.call(self) if block
+        end
+
+        #
+        # Returns all defined SQL ErrorPattern objects.
+        #
+        def ErrorPattern.patterns
+          @@patterns ||= {}
+        end
+
+        #
+        # Defines a new SQL ErrorPattern object with the given _options_.
+        #
+        def ErrorPattern.pattern(name,&block)
+          pattern = (ErrorPattern.patterns[name] ||= ErrorPattern.new(name))
+
+          block.call(pattern) if block
+          return pattern
+        end
+
+        #
+        # Returns the SQL ErrorPattern objects with the specified _names_.
+        #
+        def ErrorPattern.patterns_for(*names)
+          names.map { |name| ErrorPattern.patterns[name] }.compact
+        end
+
+        #
+        # Returns the SQL ErrorPattern objects for the dialect with the
+        # specified _name_.
+        #
+        def ErrorPattern.patterns_for_dialect(name)
+          ErrorPattern.patterns.values.select do |pattern|
+            pattern.dialect == name
+          end
+        end
+
+        #
+        # Returns the first matching Error object to the specified _data_.
+        # If no ErrorPattern objects match the specified _data_, +nil+ will
+        # be returned.
+        #
+        def ErrorPattern.match(data)
+          ErrorPattern.patterns.each_value do |pattern|
+            if (error = pattern.match(data))
+              return error
+            end
+          end
+
+          return nil
+        end
+
+        #
+        # Returns the first match between the error pattern and the specified
+        # _data_. If no matches were found +nil+ will be returned.
+        #
+        def match(data)
+          @patterns.each do |pattern|
+            if (match = pattern.match(data))
+              return Message.new(@type,@dialect,match[0])
+            end
+          end
+
+          return nil
+        end
+
+        #
+        # Returns the name of the error pattern.
+        #
+        def to_s
+          @name.to_s
+        end
+
+      end
+    end
+  end
+end
