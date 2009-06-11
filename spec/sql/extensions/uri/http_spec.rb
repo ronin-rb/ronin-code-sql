b9959e12108d07045a3132a820523ae273a472bf
postmodern <postmodern.mod3@gmail.com>
2009-06-11 16:48:03 -0700

    Added specs for URI::HTTP.

---
+++ spec/sql/extensions/uri/http_spec.rb
@@ -0,0 +1,20 @@
+require 'ronin/sql/extensions/uri/http'
+
+require 'spec_helper'
+
+describe URI::HTTP do
+  before(:all) do
+    @url = URI('http://testasp.acunetix.com/showthread.asp?id=2')
+  end
+
+  it "should determine which query params have SQL errors" do
+    @url.sql_errors.should == {'id' => '2'}
+  end
+
+  it "should determine which query params have SQL injection" do
+    inj = @url.sql_injection
+
+    inj.param.should == 'id'
+    inj.sql_options[:escape].should == '2'
+  end
+end
