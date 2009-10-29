6b95c2d40cec56e8e6615a88b4287b3ae731bbf6
postmodern <postmodern.mod3@gmail.com>
2009-10-28 20:20:11 -0700

    Put all spec helpers within the Helpers namespace.

--- spec/helpers/code.rb
+++ spec/helpers/code.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 require 'ronin/code/sql/common_dialect'
 
-include Code::SQL
+module Helpers
+  include Code::SQL
 
-def common_dialect
-  Dialect.get(:common).new
-end
+  def common_dialect
+    Dialect.get(:common).new
+  end
 
-def should_have_clause(sql,name,&block)
-  sql.has_clause?(name).should == true
-  block.call(sql.get_clause(name)) if block
+  def should_have_clause(sql,name,&block)
+    sql.has_clause?(name).should == true
+    block.call(sql.get_clause(name)) if block
+  end
 end
