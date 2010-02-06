2337c00478446497c6eb693540457a1b3c1f1372
postmodern <postmodern.mod3@gmail.com>
2010-02-05 20:19:15 -0800

    Added basic specs for Fragment.

--- spec/code/sql/fragment_spec.rb
+++ spec/code/sql/fragment_spec.rb
@@ -3,4 +3,21 @@ require 'ronin/code/sql/fragment'
 require 'spec_helper'
 
 describe Code::SQL::Fragment do
+  it "should encode fragments with no elements" do
+    frag = Code::SQL::Fragment.new([])
+
+    frag.to_s.should == ''
+  end
+
+  it "should encode fragments with one element" do
+    frag = Code::SQL::Fragment.new([:id])
+
+    frag.to_s.should == 'id'
+  end
+
+  it "should encode fragments with more than one element" do
+    frag = Code::SQL::Fragment.new([:drop_table, :users])
+
+    frag.to_s.should == 'drop_table users'
+  end
 end
