b6fa0223e3e1d0057953f3574556a391cad91b1d
Postmodern <postmodern.mod3@gmail.com>
2010-09-15 19:58:43 -0700

    Updated the URI::HTTP specs.

--- spec/sql/extensions/uri/http_spec.rb
+++ spec/sql/extensions/uri/http_spec.rb
@@ -2,48 +2,28 @@ require 'spec_helper'
 require 'ronin/sql/extensions/uri/http'
 
 describe URI::HTTP do
-  before(:all) do
-    @url = URI('http://testasp.acunetix.com/showthread.asp?id=2')
-  end
+  let(:url) { URI('http://testasp.acunetix.com/showthread.asp?id=2') }
 
   it "should determine which query params have SQL errors" do
-    pending "Spring Cleaning" do
-      raise()
-
-      @url.sql_errors.should == {'id' => '2'}
-    end
+    url.sql_errors.should == {'id' => '2'}
   end
 
-  it "should find all SQL injections" do
-    pending "Spring Cleaning" do
-      raise()
+  it "should scan the URL for all SQL injections" do
+    injections = url.sqli_scan.to_a
 
-      injections = @url.sql_injections
-      injection = injections.first
-
-      injections.length.should == 1
-
-      injection.param.should == 'id'
-      injection.sql_options[:escape].should == '2'
-    end
+    injections.length.should == 1
+    injections[0].param.should == 'id'
+    injections[0].sql_options[:escape].should == '2'
   end
 
   it "should find the first working SQL injection" do
-    pending "Spring Cleaning" do
-      raise()
+    injection = url.first_sqli
 
-      injection = @url.sql_injection
-
-      injection.param.should == 'id'
-      injection.sql_options[:escape].should == '2'
-    end
+    injection.param.should == 'id'
+    injection.sql_options[:escape].should == '2'
   end
 
   it "should determine if a URL is vulnerable to SQL injection" do
-    pending "Spring Cleaning" do
-      raise()
-
-      @url.has_sql_injections?.should == true
-    end
+    url.has_sqli?.should == true
   end
 end
