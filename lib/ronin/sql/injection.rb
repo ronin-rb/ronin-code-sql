149dbb4977732e6369d0a217859e3fc95565ba0c
Postmodern <postmodern.mod3@gmail.com>
2010-09-15 19:59:01 -0700

    Fixed a typo.

--- lib/ronin/sql/injection.rb
+++ lib/ronin/sql/injection.rb
@@ -19,7 +19,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
-require 'ronin/sql/error'
+require 'ronin/sql/errors'
 require 'ronin/code/sql'
 require 'ronin/network/mixins/http'
 require 'ronin/web/extensions/nokogiri'
