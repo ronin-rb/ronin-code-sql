12200d5aa45f5a95a244c5321045a961732ef4b6
Postmodern <postmodern.mod3@gmail.com>
2013-01-17 22:46:14 -0800

    Use generic names.

--- spec/sql/clauses_spec.rb
+++ spec/sql/clauses_spec.rb
@@ -60,22 +60,22 @@ describe SQL::Clauses do
     end
   end
 
-  include_examples "Clause", :from, :FROM, :users
-  include_examples "Clause", :into, :INTO, :users
+  include_examples "Clause", :from, :FROM, :table
+  include_examples "Clause", :into, :INTO, :table
   include_examples "Clause", :where, :WHERE, proc { id == 1 }
-  include_examples "Clause", :join, :JOIN, :users
-  include_examples "Clause", :inner_join, :"INNER JOIN", :users
-  include_examples "Clause", :left_join, :"LEFT JOIN", :users
-  include_examples "Clause", :right_join, :"RIGHT JOIN", :users
-  include_examples "Clause", :full_join, :"FULL JOIN", :users
+  include_examples "Clause", :join, :JOIN, :table
+  include_examples "Clause", :inner_join, :"INNER JOIN", :table
+  include_examples "Clause", :left_join, :"LEFT JOIN", :table
+  include_examples "Clause", :right_join, :"RIGHT JOIN", :table
+  include_examples "Clause", :full_join, :"FULL JOIN", :table
   include_examples "Clause", :on, :ON, proc { id == 1 }
-  include_examples "Clause", :union, :UNION, proc { select(:*).from(:users) }
+  include_examples "Clause", :union, :UNION, proc { select(:*).from(:table) }
   include_examples "Clause", :group_by, :"GROUP BY", [:column1, :column2]
   include_examples "Clause", :having, :HAVING, proc { max(priv) > 100 }
   include_examples "Clause", :limit, :LIMIT, 100
   include_examples "Clause", :offset, :OFFSET, 20
   include_examples "Clause", :top, :TOP, 50
-  include_examples "Clause", :into, :INTO, :users
+  include_examples "Clause", :into, :INTO, :table
   include_examples "Clause", :values, :VALUES, [1,2,3,4]
   include_examples "Clause", :default_values, :"DEFAULT VALUES"
   include_examples "Clause", :set, :SET, {:x => 1, :y => 2}
