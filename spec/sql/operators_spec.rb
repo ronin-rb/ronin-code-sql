7d6e376d56daa98ff484619ba32a9a5c45a3a11b
Postmodern <postmodern.mod3@gmail.com>
2013-01-17 22:43:39 -0800

    Added specs for Operators.

---
+++ spec/sql/operators_spec.rb
@@ -0,0 +1,80 @@
+require 'spec_helper'
+require 'ronin/sql/operators'
+
+shared_examples_for "BinaryExpr" do |method,operator=method|
+  describe "##{method}" do
+    let(:operand) { 1 }
+    let(:expr)    { subject.send(method,operand) }
+
+    it "should be a BinaryExpr" do
+      expr.should be_kind_of(SQL::BinaryExpr)
+    end
+
+    it "should set the right-hand operand" do
+      expr.right.should == operand
+    end
+
+    it "should have a '#{operator}' operator" do
+      expr.operator.should == operator
+    end
+  end
+end
+
+shared_examples_for "UnaryExpr" do |method,operator=method|
+  describe "##{method}" do
+    let(:expr)    { subject.send(method) }
+
+    it "should be a UnaryExpr" do
+      expr.should be_kind_of(SQL::UnaryExpr)
+    end
+
+    it "should set the operand" do
+      expr.operand.should == subject
+    end
+
+    it "should have a '#{operator}' operator" do
+      expr.operator.should == operator
+    end
+  end
+end
+
+describe SQL::Operators do
+  subject { Object.new.extend(described_class) }
+
+  let(:operand) { 1 }
+
+  include_examples "BinaryExpr", :*
+  include_examples "BinaryExpr", :/
+  include_examples "BinaryExpr", :%
+  include_examples "BinaryExpr", :+
+  include_examples "BinaryExpr", :-
+  include_examples "BinaryExpr", :<<
+  include_examples "BinaryExpr", :>>
+  include_examples "BinaryExpr", :&
+  include_examples "BinaryExpr", :|
+  include_examples "BinaryExpr", :<
+  include_examples "BinaryExpr", :<=
+  include_examples "BinaryExpr", :>
+  include_examples "BinaryExpr", :>=
+  include_examples "BinaryExpr", :==, :"="
+  include_examples "BinaryExpr", :!=
+  include_examples "BinaryExpr", :as, :AS
+  include_examples "BinaryExpr", :is, :IS
+  include_examples "BinaryExpr", :is_not, :"IS NOT"
+  include_examples "BinaryExpr", :like, :LIKE
+  include_examples "BinaryExpr", :glob, :GLOB
+  include_examples "BinaryExpr", :match, :MATCH
+  include_examples "BinaryExpr", :regexp, :REGEXP
+  include_examples "BinaryExpr", :in, :IN
+
+  pending "need to figure out how to send a unary operator" do
+    include_examples "UnaryExpr", :-
+    include_examples "UnaryExpr", :+
+    include_examples "UnaryExpr", :~
+    include_examples "UnaryExpr", :!
+    include_examples "UnaryExpr", :not, :NOT
+  end
+
+  include_examples "BinaryExpr", :and, :AND
+  include_examples "BinaryExpr", :or, :OR
+end
