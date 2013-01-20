c45ae54dafa0d1a40693d9f26aafef048171c0e7
Postmodern <postmodern.mod3@gmail.com>
2013-01-19 16:28:17 -0800

    Forgot to copy in the old ronin/formatting/sql specs.

---
+++ spec/formatting/sql/string_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+require 'ronin/formatting/extensions/sql/string'
+
+describe String do
+  let(:string) {'/etc/passwd' }
+  let(:sql_encoded) { '0x2f6574632f706173737764' }
+  let(:string_with_quotes) { %{"O'Brian"} }
+
+  it "should provide the #sql_escape method" do
+    string.should respond_to(:sql_escape)
+  end
+
+  it "should provide the #sql_encode method" do
+    string.should respond_to(:sql_encode)
+  end
+
+  it "should provide the #sql_decode method" do
+    string.should respond_to(:sql_decode)
+  end
+
+  describe "#sql_escape" do
+    it "should be able to single-quote escape" do
+      string_with_quotes.sql_escape(:single).should == %{'"O''Brian"'}
+    end
+
+    it "should be able to double-quote escape" do
+      string_with_quotes.sql_escape(:double).should == %{"""O'Brian"""}
+    end
+
+    it "should be able to tick-mark escape" do
+      string_with_quotes.sql_escape(:tick).should == %{`"O'Brian"`}
+    end
+  end
+
+  describe "#sql_encode" do
+    it "should be able to be SQL-hex encoded" do
+      string.sql_encode.should == sql_encoded
+    end
+
+    it "should return an empty String if empty" do
+      ''.sql_encode.should == ''
+    end
+  end
+
+  describe "#sql_decode" do
+    it "should be able to be SQL-hex decoded" do
+      encoded = string.sql_encode
+
+      encoded.should == sql_encoded
+      encoded.sql_decode.should == string
+    end
+
+    it "should be able to decode SQL comma-escaping" do
+      "'Conan O''Brian'".sql_decode.should == "Conan O'Brian"
+    end
+  end
+end
